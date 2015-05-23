3f075b0fcb4b690e224869ec674ca4c47f4aaaca
ctamas <ballin360x@gmail.com>
2015-05-23 12:33:12 +0200

    grafikon feliratok, skalazodas

--- DiffSolverCsharp/PiffTeamSolution/PiffTeam_Output/EulerMethod/MainWindow.xaml.cs
+++ DiffSolverCsharp/PiffTeamSolution/PiffTeam_Output/EulerMethod/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace PiffTeam
         const float TR = 20f; // külső hőmérséklet
         const float k = 0.07f; // hűlési konstans
         readonly static float Zoom = 5;
-        const int n = 130;               
+        const int n = 80;               // Y koordinata terjedelme (altalaban ido) 
         static int co = -1, nr = 0;
    
         public MainWindow()
@@ -62,8 +62,11 @@ namespace PiffTeam
             }
 
         public void Draw()
-        {       // co azt azonosítja hogy draw button katt esetén melyik szín jön most, X
-                // enbabled: pipa kivétel esetén el kellene tünjön(mindet újra rajzolva kivéve disabled-et)
+        {       // co azt azonosítja hogy draw button katt esetén melyik szín jön most, it coord elso dimenzioja
+                // isChecked: pipa kivétel esetén el kellene tünjön(mindet újra rajzolva kivéve disabled-et)
+            float distance, min, max;
+            GetMaxMin(out max, out min, out distance);
+            float diff = (100 - max);
             if (l1check.IsChecked == true)
                 {
                     for (int x = 0; x == Coords[0, 2, x]; x++) //  végig megy az elemeken, sorszám szerint
@@ -72,11 +75,12 @@ namespace PiffTeam
                         {
                             Stroke = System.Windows.Media.Brushes.DarkBlue,
                             StrokeThickness = 4,
-                            X1 = (int)Zoom * Coords[0, 0, x] + 50, // a 50 azért kell, hogy elférjen a skála
-                            Y1 = (int)Zoom * (T0 - Coords[0, 1, x]),
-                            X2 = (int)Zoom * Coords[0, 0, x] + 2 + 50, // a 50 azért kell, hogy elférjen a skála, a 2 pedig, hogy legyen kiterjedése a vonalnak
-                            Y2 = (int)Zoom * (T0 - Coords[0, 1, x]) + 2
+                            X1 = (int)Zoom * (Coords[0, 0, x] / ((float)n / 100)) + 50, // a 50 azért kell, hogy elférjen a skála
+                            Y1 = (int)Zoom * (100 - (Coords[0, 1, x]-min) / (distance / 100)) + 15,  // az osztas barmilyen intervallumot 100as skalahoz igazit, a kivonas pedig grafikon tetejehez igazitja
+                            X2 = (int)Zoom * (Coords[0, 0, x] / ((float)n / 100)) + 52, // a 50 azért kell, hogy elférjen a skála, a 2 pedig, hogy legyen kiterjedése a vonalnak
+                            Y2 = (int)Zoom * (100 - (Coords[0, 1, x] - min) / (distance / 100)) + 17
                         });
+                        System.Diagnostics.Debug.WriteLine(" X1 " + (Coords[0, 0, x] / ((float)n / 100)) + " Y1 " + ((100 - (Coords[0, 1, x] - min) / (distance / 100))));
                     }
                 }
             if (l2check.IsChecked == true)
@@ -88,9 +92,9 @@ namespace PiffTeam
                             Stroke = System.Windows.Media.Brushes.DarkGreen,
                             StrokeThickness = 4,
                             X1 = (int)Zoom * Coords[1, 0, x] + 50,
-                            Y1 = (int)Zoom * (T0 - Coords[1, 1, x]),
+                            Y1 = (int)Zoom * (100 - (Coords[1, 1, x] - min) / (distance / 100)) + 15, 
                             X2 = (int)Zoom * Coords[1, 0, x] + 2 + 50,
-                            Y2 = (int)Zoom * (T0 - Coords[1, 1, x]) + 2
+                            Y2 = (int)Zoom * (100 - (Coords[1, 1, x] - min) / (distance / 100)) + 17
                         });
                     }
                 }
@@ -103,31 +107,80 @@ namespace PiffTeam
                             Stroke = System.Windows.Media.Brushes.DarkRed,
                             StrokeThickness = 4,
                             X1 = (int)Zoom * Coords[2, 0, x] + 50,
-                            Y1 = (int)Zoom * (T0 - Coords[2, 1, x]),
+                            Y1 = (int)Zoom * (100 - (Coords[2, 1, x] - min) / (distance / 100)) + 15,
                             X2 = (int)Zoom * Coords[2, 0, x] + 2 + 50,
-                            Y2 = (int)Zoom * (T0 - Coords[2, 1, x]) + 2
+                            Y2 = (int)Zoom * (100 - (Coords[2, 1, x] - min) / (distance / 100)) + 17
                         });
                     }
                 }
         }
 
+
+        public void GetMaxMin(out float max, out float min, out float distance)
+        {
+            min = Coords[0, 1, 0]; max = Coords[0, 1, 0];
+            for (int x = 0; x == Coords[0, 2, x]; x++)
+            {       // min es max tengelyertekek X síkon elso grafikon szerint rajzolodik fel
+                if (Coords[0, 1, x] < min) min = Coords[0, 1, x];
+                if (Coords[0, 1, x] > max) max = Coords[0, 1, x];
+            }
+            distance = Math.Abs(max - min) ;  //  tavolsag min és max kozott
+        }
+
+
         private void DisplayScale()
         {
-          for (int i = (int)TR; i <= T0; i += 10)
+            float distance, min, max;
+            GetMaxMin(out max, out min, out distance);
+
+            System.Diagnostics.Debug.WriteLine("min " + min + " max " + max + "\t");
+
+            //szazalekot szamol Y tengelyre 100% a max ertek 0% a miniimum. pl 50% 0.5 szorosa 100nak
+            int j=0;
+            for (float i = min; j <= 100; i +=(distance/10))
+
             {
 
+                canvEuler.Children.Add(new Line                 // vonal racs vizszintes
+                {
+                    Stroke = System.Windows.Media.Brushes.Gray,
+                    StrokeThickness = 0.5,
+                    X1 = 1,
+                    X2 = 2000,
+                    Y1 = j * 5 + 16,
+                    Y2 = j * 5 + 16,
+                });
+              //distance=
                 TextBlock testc = new TextBlock();
-                testc.Text = i.ToString()+"°C";
-                Canvas.SetTop(testc, (int)Zoom * (T0 - i));
+            //    if(j==0)testc.Text = " " + Math.Round(( min), 2) + "°C";
+            //    else if(j==100)testc.Text = " " + Math.Round(( max), 2) + "°C";
+           //     else 
+                    testc.Text = " " + Math.Round(( i), 2) + "°C";
+                    
+                Canvas.SetTop(testc, (int)Zoom * (100 - j));
                 canvEuler.Children.Add(testc);
-            }
-            for (int i = 0; i <= n; i += 10)
+                System.Diagnostics.Debug.WriteLine(" j " + j + " i " + i);
+                j += 10;
+            }               // n
+            j = 0;
+            for (int i = 0; i <= n; i += n/10)
             {
+                
+                canvEuler.Children.Add(new Line                 // vonal racs fuggoleges
+                {
+                    Stroke = System.Windows.Media.Brushes.Gray,
+                    StrokeThickness = 0.5,
+                    X1 = 50+j * 5,
+                    X2 = 50+j * 5,
+                    Y1 = 1,
+                    Y2 = 2000,
+                });
 
                 TextBlock testv = new TextBlock();
                 testv.Text = i.ToString() + "s";
-                Canvas.SetLeft(testv, (int)Zoom* i+40);
+                Canvas.SetLeft(testv, (int)Zoom* j+53);
                 canvEuler.Children.Add(testv);
+                j += 10;
             }
         }
 
@@ -138,13 +191,12 @@ namespace PiffTeam
                 float delta = Convert.ToSingle(TxDelta.Text);
                 if (co == 2) co = -1; // ciklusvaltozo, a kirajzolt fgv azosítója >> színe
                 co++;
-
+           //     Console.WriteLine("x: " + Cursor.Position.X + " y: " + Cursor.Position.Y);
+                func f = new func(NewtonCooling);
+                Euler(f, T0, n, delta, co);
                 if (co == 0) l1check.IsChecked = true;
                 if (co == 1) l2check.IsChecked = true;
                 if (co == 2) l3check.IsChecked = true;
-                func f = new func(NewtonCooling);
-                Euler(f, T0, n, delta, co);
-                Draw();
             }
             catch(Exception)
             {
